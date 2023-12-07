9cea940cab77f1e7672fafccdb424d585f46efcf
Mugesh R <151606977+mugesh-88@users.noreply.github.com>
2023-12-08 00:23:27 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-import Grid from "../Grid.js";
-import Tile from "../Tile.js";
+import Grid from "./Grid.js";
+import Tile from "./Tile.js";
 const gameBoard = document.getElementById("game-board");
 const grid= new Grid(gameBoard);
 //console.log(grid.randomEmptyCell());
